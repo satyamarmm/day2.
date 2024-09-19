b3c2bb2fc5d4eba1da4a6793de17477e7b47f728
Amarnath Manchimsetti <150233430+satyamarmm@users.noreply.github.com>
2024-09-19 09:26:59 +0530

    Add files via upload

---
+++ day2.js
@@ -0,0 +1,27 @@
+a=5;
+b=8;
+c=10;
+
+console.log(a);
+console.log(b);
+
+var a="amarnath";
+
+console.log(a);
+console.log(b);
+
+console.log(a+b);
+ 
+age=21;
+
+if(age=>18)
+    {
+    console.log("ready to vote");
+}
+else
+{
+console.log("not ready to vote");
+}
+
+
+
